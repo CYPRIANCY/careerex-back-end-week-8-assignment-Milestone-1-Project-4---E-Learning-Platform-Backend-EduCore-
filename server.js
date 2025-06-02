5ce2a7cbcd62dd7b771f663790ad1458cb236df7
CYPRIANCY <akpensesughcyprian@gmail.com>
2025-06-02 16:36:03 +0100

    full backEnd

--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import connectDB from './config/db.js';
-// import authRoutes from './routes/authRoutes.js';
-// import userRoutes from './routes/userRoutes.js';
-// import courseRoutes from './routes/courseRoutes.js';
-// import enrollmentRoutes from './routes/enrollmentRoutes.js';
-// import paymentRoutes from './routes/paymentRoutes.js';
-// import certificateRoutes from './routes/certificateRoutes.js';
-// import messageRoutes from './routes/messageRoutes.js';
-// import adminRoutes from './routes/adminRoutes.js';
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-// app.use(express.json());
-
-// app.use('/api/auth', authRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/api/courses', courseRoutes);
-// app.use('/api/enrollments', enrollmentRoutes);
-// app.use('/api/payments', paymentRoutes);
-// app.use('/api/certificates', certificateRoutes);
-// app.use('/api/messages', messageRoutes);
-// app.use('/api/admin', adminRoutes);
-
-// app.get('/', (req, res) => res.send('EduCore API running'));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
